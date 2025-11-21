d7852234bd7708892b01fc3021b2a3fd172a087d
Riansyah <syahryan547@gmail.com>
2025-11-21 12:55:35 +0700

    Create Soal10.java

---
+++ Soal10.java
@@ -0,0 +1,16 @@
+package Tugas6;
+
+import java.util.ArrayList;
+
+public class Soal10 {
+    public static void main(String[] args) {
+        ArrayList<Object> data = new ArrayList<>();
+
+        data.add(19);
+        data.add(0.2);
+        data.add('C');
+        data.add("Java");
+        data.add(true);
+
+    }
+}
