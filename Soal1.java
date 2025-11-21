93fb564d03adf363785a8b7a0b67010c9b27b405
Riansyah <syahryan547@gmail.com>
2025-11-21 12:48:00 +0700

    Create Soal1.java

---
+++ Soal1.java
@@ -0,0 +1,9 @@
+package Tugas6;
+
+public class soal1 {
+    public static void main(String[] args) {
+        int angka[] = {10,20,30,40,50};  
+        int[] nilai = {10,20,30,40,50};
+        int[] data = new int[10];
+    }
+}
